79b4370eef83efa8f300c91eb0d17e6a61ee8d3c
thytran142 <thytran142@hotmail.com>
2020-11-22 15:11:42 -0500

    [Register HTML] Add register html and register.css page.

---
+++ process.php
@@ -0,0 +1,8 @@
+<?php
+// ------------------------------------------------------------------------------
+// Assignment (include number)
+// todo fill your name here
+// Written by: (include your name (s) and student id(s))
+// For SOEN 287 Section (your section) – Fall 2020
+// -----------------------------------------------------------------------------
+?>
